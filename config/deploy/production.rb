9cd2b98ba7526c6a4a26b4d29f47117f806614dd
Isaac Askew <isaac@share.local>
2019-12-03 19:49:17 -0600

    Fixing creds

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{ec2-user@ec2-18-205-153-201.compute-1.amazonaws.com}
+role :web, %w{ec2-user@ec2-18-205-153-201.compute-1.amazonaws.com}
+role :db,  %w{ec2-user@ec2-18-205-153-201.compute-1.amazonaws.com}
 
 
 
@@ -41,11 +41,11 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/Users/isaac/Downloads/isaac_blog.pem),
+   forward_agent: false,
+   auth_methods: %w(publickey password)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
